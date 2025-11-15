1fe0893313bf6b999e6d279ecb061a11f3d1bf0d
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2025-11-15 18:29:37 +0700

    feat(Esp32): Add main.cpp code

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,18 +1,68 @@
-#include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// mengimport library dan file yang di butuhkan
+#include <Arduino.h> //library arduino wajib
+#include <WiFi.h> // library untuk menghubungkan esp32 ke wifi
+#include <Firebase_ESP_Client.h> // library untuk menghubungkan esp dengan firestore
+#include <addons/TokenHelper.h> // library untuk tambahan dari firebase untuk mengurus token authentikasi
+#include "secrets.h" // file untuk menyimpan kunci WiFi, dan konfigurasi firebase
+
+
+// dikarenakan saya membuat file define WIFI_SSID, WIFI_PASSWORD, API_KEY, DATABASE_URL 
+// di file lain yang saya sembunyikan dengan .gitignore
+// isinya seperti codingan di bawah ini, kalian bisa menaruh codingannya di bawah ini atau di file lain 
+
+// #define WIFI_SSID "davinnn" Sesuaikan dengan nama wifi saudara"
+// #define WIFI_PASSWORD "1paranoia" sesuaikan dengan password wifi saudara"
+
+// #define API_KEY "xxxxx" sesuaikan dengan API_KEY saudara yang terdapat di firebase
+// #define DATABASE_URL "xxxxx" Sesuaikan dengan link yang terdapat pada database realtime di firestore
+
+//membuat objek untuk firebase
+FirebaseConfig config;
+FirebaseAuth auth;
+FirebaseData fbdo;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200); // untuk melihat di serial monitor
+  Serial.println("BOOT OK");
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); // membuat esp terhubung dengan wifi
+  // membuat perulangan untuk memastikan wifi terhubung
+  Serial.print("Menghubungkan WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+
+  // jika looping di atas sudah selesai dan terhubung maka akan menampilkan WiFi connected dan ip nya
+  Serial.println("\nWiFi connected");
+  Serial.println(WiFi.localIP());
+
+  //mengisi object yang sudah kita deklarasikan dengan data kita
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+
+  // set atuh, ke mode test
+  config.signer.test_mode = true;
+
+  // memulai koneksi ke firebase dan Memberi tahu library Firebase agar otomatis mencoba menyambung ulang WiFi jika koneksi tiba-tiba putus.
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  
+  if (Firebase.ready()) { // Mengecek, "Apakah Firebase sudah siap menerima perintah
+    if (Firebase.RTDB.setString(&fbdo, "/TEST", "ESP8266_OK")) { // mengirim data ke firebase dengan path /TEST yang berisi ESP8266_OK
+      Serial.println("Kirim data BERHASIL!");
+    } else {
+      Serial.print("Gagal: ");
+      Serial.println(fbdo.errorReason());
+    }
+  } else {
+    Serial.println("Firebase belum siap...");
+  }
+  
+  // membuat delay 3 detik dan mengirimkannya kembali lagi
+  delay(3000);
 }
