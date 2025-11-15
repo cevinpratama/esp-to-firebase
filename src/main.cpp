6977c0eb07aa24e3c860ae4fb2e5193743716544
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2025-11-15 12:48:03 +0700

    feat(main.cpp): Add connection esp 8266

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,46 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include <addons/TokenHelper.h>
+#include "secrets.h"
 
-// put function declarations here:
-int myFunction(int, int);
+FirebaseConfig config;
+FirebaseAuth auth;
+FirebaseData fbdo;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("BOOT OK");
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Menghubungkan WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println("\nWiFi connected");
+  Serial.println(WiFi.localIP());
+
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+
+  config.signer.test_mode = true;
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (Firebase.ready()) {
+    if (Firebase.RTDB.setString(&fbdo, "/TEST", "ESP8266_OK")) {
+      Serial.println("Kirim data BERHASIL!");
+    } else {
+      Serial.print("Gagal: ");
+      Serial.println(fbdo.errorReason());
+    }
+  } else {
+    Serial.println("Firebase belum siap...");
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(3000);
 }
